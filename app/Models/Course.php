f8e69a619f254cef27333b751895456e86a03cb8
BePsvPT <bepsvpt@protonmail.com>
2018-10-06 21:49:33 +0800

    setup laravel scout

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Laravel\Scout\Searchable;
+
 class Course extends Model
 {
+    use Searchable;
+
     /**
      * The comments that belong to the course.
      *
@@ -13,6 +17,26 @@ class Course extends Model
     {
         return $this->belongsToMany(Comment::class)
             ->withPivot('professor_id');
+    }
+
+    /**
+     * Get the department that owns the course.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    /**
+     * Get the dimension that owns the course.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function dimension()
+    {
+        return $this->belongsTo(Dimension::class);
     }
 
     /**
@@ -34,5 +58,38 @@ class Course extends Model
     public function semesters()
     {
         return $this->belongsToMany(Semester::class);
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        foreach (['department', 'dimension', 'professors'] as $relation) {
+            if (!$this->relationLoaded($relation)) {
+                $this->load($relation);
+            }
+        }
+
+        return [
+            'code' => $this->code,
+            'name' => $this->name,
+            'college' => $this->department->college,
+            'department' => $this->department->name,
+            'dimension' => optional($this->dimension)->name,
+            'professors' => $this->professors->pluck('name')->toArray(),
+        ];
+    }
+
+    /**
+     * Get the value used to index the model.
+     *
+     * @return mixed
+     */
+    public function getScoutKey()
+    {
+        return $this->code;
     }
 }
