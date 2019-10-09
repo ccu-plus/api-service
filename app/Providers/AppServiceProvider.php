c67f2d897611a6c04c8dd385ff3124e3ed298d07
BePsvPT <bepsvpt@protonmail.com>
2019-10-09 23:19:31 +0800

    add captcha singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Gregwar\Captcha\CaptchaBuilder;
+use Gregwar\Captcha\PhraseBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->captcha();
+    }
+
+    /**
+     * Register captcha service.
+     *
+     * @return void
+     */
+    protected function captcha(): void
+    {
+        $this->app->singleton('captcha', function () {
+            $phrase = new PhraseBuilder(5, '0123456789');
+
+            $captcha = new CaptchaBuilder(null, $phrase);
+
+            $captcha->setIgnoreAllEffects(true);
+
+            $captcha->setMaxAngle(35);
+
+            return $captcha;
+        });
     }
 }
