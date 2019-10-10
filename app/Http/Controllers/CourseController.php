c74717aa73be454e01b5d2062723226c53f9fe3f
BePsvPT <bepsvpt@protonmail.com>
2019-10-10 21:04:10 +0800

    update course controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,11 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\Semester;
+use App\Transformers\CourseTransformer;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 class CourseController extends Controller
 {
-    public function search()
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function search(Request $request): JsonResponse
     {
-        //
+        $keyword = '';
+
+        foreach (['college', 'department', 'dimension', 'keyword'] as $filter) {
+            if (!empty($input = $request->input($filter))) {
+                $keyword = trim(sprintf('%s %s', $keyword, $input));
+            }
+        }
+
+        $key = sprintf('search-%s', base64_encode($keyword));
+
+        $courses = Cache::remember($key, 60 * 60, function () use ($keyword) {
+            return Course::search($keyword)
+                ->take(300)
+                ->get()
+                ->load('department', 'dimension', 'professors')
+                ->loadCount(['semesters', 'semesters as newest_semesters_count' => function (Builder $query) {
+                    $query->where('semesters.id', '=', Semester::newest()->getKey());
+                }]);
+        });
+
+        return fractal()
+            ->collection($courses->loadCount('comments'))
+            ->transformWith(new CourseTransformer)
+            ->respond();
     }
 
     public function waterfall()
@@ -14,8 +50,19 @@ class CourseController extends Controller
         //
     }
 
-    public function show()
+    /**
+     * @param string $code
+     *
+     * @return JsonResponse
+     */
+    public function show(string $code): JsonResponse
     {
-        //
+        $course = Course::with('department', 'dimension', 'semesters', 'professors', 'comments')
+            ->where('code', '=', $code)
+            ->firstOrFail();
+
+        return fractal($course)
+            ->transformWith(new CourseTransformer)
+            ->respond();
     }
 }
