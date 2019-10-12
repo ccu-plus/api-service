ac04ec52f0baaa51024491cedda3671a5882afc7
BePsvPT <bepsvpt@protonmail.com>
2019-10-12 22:44:18 +0800

    add comment transformer

---
+++ app/Transformers/CommentTransformer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Transformers;
+
+use CCUPLUS\EloquentORM\Comment;
+use League\Fractal\TransformerAbstract;
+
+class CommentTransformer extends TransformerAbstract
+{
+    /**
+     * Comment transformer.
+     *
+     * @param Comment $comment
+     *
+     * @return array
+     */
+    public function transform(Comment $comment): array
+    {
+        return [
+            'user' => ($comment->anonymous || $comment->trashed()) ? null : $comment->user->nickname,
+            'professor' => optional($comment->professor)->name,
+            'content' => $comment->trashed() ? null : $comment->content,
+            'commented_at' => $comment->created_at->toDateTimeString(),
+            'deleted' => $comment->trashed(),
+            'comments' => $comment->trashed() ? [] : fractal()->collection($comment->comments)->transformWith(new CommentTransformer)->toArray()['data'],
+        ];
+    }
+}
