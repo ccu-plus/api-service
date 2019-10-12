7c58456b2b2c1e1e55116ba2c96dd305f50ebb05
BePsvPT <bepsvpt@protonmail.com>
2019-10-12 22:44:27 +0800

    update comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Transformers\CommentTransformer;
+use CCUPLUS\EloquentORM\Course;
+use Illuminate\Http\JsonResponse;
+
 class CommentController extends Controller
 {
-    public function index()
+    /**
+     * 取得課程評論.
+     *
+     * @param string $code
+     *
+     * @return JsonResponse
+     */
+    public function index(string $code): JsonResponse
     {
-        //
+        /** @var Course $course */
+
+        $course = Course::query()
+            ->where('code', '=', $code)
+            ->firstOrFail();
+
+        $comments = $course->comments()
+            ->with('professor', 'comments')
+            ->withTrashed()
+            ->latest()
+            ->get();
+
+        return fractal()
+            ->collection($comments)
+            ->transformWith(new CommentTransformer)
+            ->respond();
     }
 
-    public function store()
-    {
-        //
-    }
-
-    public function like()
+    public function store(string $code)
     {
         //
     }
