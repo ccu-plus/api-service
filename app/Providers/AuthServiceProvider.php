6fc0c23e83ebcaaf4dbf7f3c6af011d879162258
BePsvPT <bepsvpt@protonmail.com>
2019-10-15 20:15:22 +0800

    update auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use CCUPLUS\EloquentORM\User;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Http\Request;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,15 +16,52 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Here you may define how you wish users to be authenticated for your Lumen
-        // application. The callback which receives the incoming request instance
-        // should return either a User instance or null. You're free to obtain
-        // the User instance via an API token or any other method necessary.
+        /** @var AuthManager $auth */
 
-        $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+        $auth = $this->app['auth'];
+
+        $auth->viaRequest('api', function (Request $request) {
+            $token = $this->apiTokenToUserToken($request->header('api-token'));
+
+            if ($token === false) {
+                return null;
             }
+
+            return User::query()
+                ->where('token', '=', $token)
+                ->first();
         });
+    }
+
+    /**
+     * Verify api token and retrieve token from it.
+     *
+     * @param mixed $apiKey
+     *
+     * @return bool|string
+     */
+    protected function apiTokenToUserToken($apiKey)
+    {
+        if (empty($apiKey) || !is_string($apiKey)) { // 確認 apiKey 為字串
+            return false;
+        } else if (strpos($apiKey, '.') === false) { // 確認 apiKey 含有「.」
+            return false;
+        }
+
+        [$token, $hmac] = explode('.', $apiKey);
+
+        if (empty($token) || empty($hmac)) { // 確認 token 和 hmac 不為空
+            return false;
+        } else if (false === ($hmac = base64_decode($hmac, true))) { // 確認 hmac 能正確 base64 decode
+            return false;
+        }
+
+        $known = hash_hmac('md5', $token, env('APP_KEY'), true);
+
+        if (hash_equals($known, $hmac) === false) { // 確認 hash mac 一致
+            return false;
+        }
+
+        return $token;
     }
 }
