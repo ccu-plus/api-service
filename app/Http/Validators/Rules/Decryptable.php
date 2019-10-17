10d6775c58eb7d495c4395c7d41cf182f2b2dbf6
BePsvPT <bepsvpt@protonmail.com>
2019-10-17 22:19:18 +0800

    add decryptable validation rule

---
+++ app/Http/Validators/Rules/Decryptable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Validators\Rules;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Contracts\Validation\Rule;
+
+final class Decryptable implements Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (!is_string($value)) {
+            return false;
+        }
+
+        try {
+            decrypt($value);
+        } catch (DecryptException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string|array
+     */
+    public function message()
+    {
+        return 'Invalid payload.';
+    }
+}
