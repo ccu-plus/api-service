b3ec08b9a084a8668374382fd199abe9559abd59
BePsvPT <bepsvpt@protonmail.com>
2019-10-19 00:51:44 +0800

    add token to comment transformer

--- app/Transformers/CommentTransformer.php
+++ app/Transformers/CommentTransformer.php
@@ -3,6 +3,7 @@
 namespace App\Transformers;
 
 use CCUPLUS\EloquentORM\Comment;
+use Illuminate\Support\Facades\Cache;
 use League\Fractal\TransformerAbstract;
 
 class CommentTransformer extends TransformerAbstract
@@ -28,6 +29,31 @@ class CommentTransformer extends TransformerAbstract
                 'name' => $comment->course->name,
                 'department' => $comment->course->department->name,
             ],
+            'token' => $this->token($comment->id),
         ];
+    }
+
+    /**
+     * Random token.
+     *
+     * @param int $key
+     *
+     * @return string
+     */
+    protected function token(int $key): string
+    {
+        try {
+            $nonce = bin2hex(random_bytes(6));
+        } catch (\Exception $e) {
+            [$micro, $time] = explode(' ', microtime());
+
+            $dec = intval($micro * (10 ** 18)) + intval($time);
+
+            $nonce = substr(str_shuffle(dechex($dec)), 0, 12);
+        }
+
+        Cache::put($nonce, $key, 60 * 60 * 6);
+
+        return $nonce;
     }
 }
