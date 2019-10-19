b4bf0f579c8c7dd8a804e2acd1d5c1221d100f15
BePsvPT <bepsvpt@protonmail.com>
2019-10-19 16:48:09 +0800

    add more space

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,14 @@ use Laravel\Lumen\Routing\Router;
 
 $router->group(['prefix' => 'courses'], function (Router $router) {
     $router->get('search', 'CourseController@search');
+
     $router->get('comments', 'CommentController@latest');
+
     $router->get('{code}', 'CourseController@show');
 
     $router->group(['prefix' => '{code}/comments'], function (Router $router) {
         $router->get('/', 'CommentController@index');
+
         $router->post('/', [
             'middleware' => ['auth'],
             'uses' => 'CommentController@store',
@@ -24,7 +27,9 @@ $router->group(['prefix' => 'account', 'middleware' => ['auth']], function (Rout
 
 $router->group(['prefix' => 'auth'], function (Router $router) {
     $router->post('sign-in', 'AuthController@signIn');
+
     $router->post('sign-up', 'AuthController@signUp');
+
     $router->post('sign-out', [
         'middleware' => ['auth'],
         'uses' => 'AuthController@signOut',
