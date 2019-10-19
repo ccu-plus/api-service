9546f48872376f831cda0f4347afd9ae971c9327
BePsvPT <bepsvpt@protonmail.com>
2019-10-19 16:41:52 +0800

    add auth middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,24 @@ $router->group(['prefix' => 'courses'], function (Router $router) {
 
     $router->group(['prefix' => '{code}/comments'], function (Router $router) {
         $router->get('/', 'CommentController@index');
-        $router->post('/', 'CommentController@store');
+        $router->post('/', [
+            'middleware' => ['auth'],
+            'uses' => 'CommentController@store',
+        ]);
     });
 });
 
-$router->group(['prefix' => 'account'], function (Router $router) {
+$router->group(['prefix' => 'account', 'middleware' => ['auth']], function (Router $router) {
     $router->get('profile', 'AccountController@profile');
 });
 
 $router->group(['prefix' => 'auth'], function (Router $router) {
     $router->post('sign-in', 'AuthController@signIn');
     $router->post('sign-up', 'AuthController@signUp');
-    $router->post('sign-out', 'AuthController@signOut');
+    $router->post('sign-out', [
+        'middleware' => ['auth'],
+        'uses' => 'AuthController@signOut',
+    ]);
 });
 
 $router->get('captcha', 'BaseController@captcha');
